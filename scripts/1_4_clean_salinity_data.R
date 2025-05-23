08ee65e8bc91aab6aa3cc3afeec8d1798c3be2bd
Peter Regier <peter.regier@pnnl.gov>
2025-05-23 15:22:12 -0600

    moving scripts around

---
+++ scripts/1_4_clean_salinity_data.R
@@ -0,0 +1,9 @@
+## This script will explore algorithmically cleaning up the salinity data, and 
+## potentially using data to impute gaps
+##
+## 2025-05-23
+## Peter Regier
+##
+# ########### #
+# ########### #
+
